dec925b00e34839874d4bdeb7f2831088390da97
kozzzz-07 <kozzzz-07@users.noreply.github.com>
2025-08-13 22:53:00 +0900

    createServer

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import http from "node:http";
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+    "accept-charset": "*",
+  });
+  res.end(
+    JSON.stringify({
+      data: "Hello World!",
+    })
+  );
+});
+
+server.listen(8080, () => {
+  console.log("The http server is listening on port 8080");
+});
